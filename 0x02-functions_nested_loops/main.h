9b42519eeeab8b9d728050084e56ac03ea8a55c2
FisayoOladejo <oladejofisayo1@gmail.com>
2022-09-13 15:58:28 -0700

    program that prints _putchar, followed by a new line

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,7 @@
 #ifndef HOLBERTON_H_INCLUDED                                                                                                            
 #define HOLBERTON_H_INCLUDED      
 
-int_putchar(char);                                                                                                                      
-void print_alphabet(void);                                                                                                              
+int _putchar(char);                                                                                                                     void print_alphabet(void);                                                                                                              
 void print_alphabet_x10(void);                                                                                                          
 int _islower(int c); 
 int _isalpha(int c);
