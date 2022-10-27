edfe90bf19ee06ae8cb50afad9073560dcabc48c
FisayoOladejo <oladejofisayo1@gmail.com>
2022-10-27 13:42:12 -0700

    Write a function that returns the value of a bit at a given index

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,21 +8,7 @@
 
 void print_binary(unsigned long int n)
 {
-	int c = 0;
-	int bit = sizeof(n) * 8 - 1;
-
-	if (n == 0)
-		_putchar('0');
-
-	while (bit >= 0)
-	{
-		if (n >> bit & 1)
-		{
-			_putchar('1');
-			c++;
-		}
-		else if (c)
-			_putchar('0');
-		bit--;
-	}
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 }
